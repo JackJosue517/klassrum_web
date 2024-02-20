6570e808b8539e916faf00d1ac882bd472b2ddd0
JackJosue517 <jackjosue517@gmail.com>
2024-02-20 09:44:50 +0000

    Mise en place de la structure et configuration de Klassrum-Web

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:klassrum_web/ui/screen/app_screen.dart';
+import 'package:klassrum_web/ui/screens/home.dart';
 
 void main() {
   runApp(const MyApp());
@@ -18,7 +18,7 @@ class MyApp extends StatelessWidget {
         useMaterial3: true,
       ),
       debugShowCheckedModeBanner: false,
-      home: const MainScreen(),
+      home: const HomeScreen(),
     );
   }
 }
